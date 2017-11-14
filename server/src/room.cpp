e750ebdee06d3bc2e41a37342db1ec781bf599a2
Zed <clement.francois@epitech.eu>
2017-11-14 22:55:48 +0100

    change CMakeLists.txt file

--- server/src/room.cpp
+++ server/src/room.cpp
@@ -11,8 +11,19 @@ protocol::serialize::StatusCode Room::insert_viewer(std::size_t viewerId) {
   auto it = std::find(m_viewer_id.begin(), m_viewer_id.end(), viewerId);
   if (it == m_viewer_id.end()) {
     m_viewer_id.push_back(viewerId);
+    return protocol::serialize::StatusCode::CLIENT_CONNECTED;
   } else {
+    return protocol::serialize::StatusCode::USER_ALREADY_IN_ROOM;
+  }
+}
 
+protocol::serialize::StatusCode Room::insert_player(std::size_t viewerId) {
+  auto it = std::find(m_player_id.begin(), m_player_id.end(), viewerId);
+  if (it == m_player_id.end()) {
+    m_player_id.push_back(viewerId);
+    return protocol::serialize::StatusCode::CLIENT_CONNECTED;
+  } else {
+    return protocol::serialize::StatusCode::USER_ALREADY_IN_ROOM;
   }
 }
 
